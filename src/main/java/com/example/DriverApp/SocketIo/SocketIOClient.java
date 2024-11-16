05127478ddb5b19763729106e40db59a0f3e5a2e
RAYSONBARAKA <barakarayson6@gmail.com>
2024-11-15 23:58:34 -0800

    server

--- src/main/java/com/example/DriverApp/SocketIo/SocketIOClient.java
+++ src/main/java/com/example/DriverApp/SocketIo/SocketIOClient.java
@@ -3,8 +3,6 @@ package com.example.DriverApp.SocketIo;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -14,39 +12,37 @@ import java.net.URISyntaxException;
 @Configuration
 public class SocketIOClient {
 
-    private static final Logger logger = LoggerFactory.getLogger(SocketIOClient.class);
-
-    @Bean
-    @Scope("singleton")
-    public Socket createSocket() {
+     @Bean
+    @Scope("singleton") 
+    public Socket creaSocket() {
         try {
-            // Configure options
-            IO.Options options = new IO.Options();
-            options.query = "deviceToken=yourDeviceToken";  
-            options.reconnection = true;
-            options.reconnectionAttempts = 5;
-            options.reconnectionDelay = 2000;
-
-            // Create a Socket.IO connection
-            Socket socket = IO.socket("https://webserver-xqjo.onrender.com", options);
+            // Create a Socket.IO connection to the server
+            Socket socket = IO.socket("https://webserver-xqjo.onrender.com");
 
             // Listen for connection
-            socket.on(Socket.EVENT_CONNECT, args -> {
-                logger.info("Connected to the server!");
-                socket.emit("message", "Hello from Java client!");
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    System.out.println("Connected to the server!");
+                    socket.emit("message", "Hello from Java client!"); 
+                }
             });
 
-            // Listen for disconnection
-            socket.on(Socket.EVENT_DISCONNECT, args -> logger.warn("Disconnected from the server."));
+            // Listen for "message" event from server
+            socket.on("message", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    System.out.println("Message from server: " + args[0]);
+                }
+            });
 
-            // Listen for connection errors
-            socket.on(Socket.EVENT_CONNECT_ERROR, args -> logger.error("Connection error: " + args[0]));
-
-            socket.on(Socket.EVENT_CONNECT_TIMEOUT, args -> logger.error("Connection timed out."));
-
-            // Listen for custom events
-            socket.on("message", args -> logger.info("Message from server: {}", args[0]));
-            socket.on("rideAccepted", args -> logger.info("Ride Accepted Notification: {}", args[0]));
+            // Listen for the custom "rideAccepted" event
+            socket.on("rideAccepted", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    System.out.println("Ride Accepted Notification: " + args[0]);
+                }
+            });
 
             // Connect to the server
             socket.connect();
@@ -54,8 +50,9 @@ public class SocketIOClient {
             return socket;
 
         } catch (URISyntaxException e) {
-            logger.error("URI syntax error during socket initialization", e);
-            return null;
+            e.printStackTrace();
         }
+        return null; 
     }
+
 }
